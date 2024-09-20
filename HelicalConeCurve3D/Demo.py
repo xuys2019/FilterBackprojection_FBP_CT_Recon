53b5a724a03458c295bc35ad664fc3f63c6436b7
xuys2019 <55818194+xuys2019@users.noreply.github.com>
2024-09-20 11:34:04 -0400

    3D Reconstruction

---
+++ HelicalConeCurve3D/Demo.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import scipy
+from pyfiles.HelicalEquiAngular import HelicalEquiAngular
+
+
+yaml_path = './config/config.yaml'
+
+## Reconstruction 
+RecImg = HelicalEquiAngular(yaml_path)
+
+# # save result
+dataNew = './data/reconstruction/HelicalReconCurve.mat'
+scipy.io.savemat(dataNew,
+             {'RecImg': RecImg})
+
+plt.figure()
+plt.imshow(RecImg[:,:,32], cmap='gray')
+plt.show()
